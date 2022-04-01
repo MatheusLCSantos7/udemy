82352b5576451a3245603cdf2609121d12073553
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-04-01 11:16:44 -0300

    back

---
+++ ArrowFunc.js
@@ -0,0 +1,14 @@
+const raiz = (n) =>  n** 0.5;
+
+
+console.log(raiz(9))
+console.log(raiz(16))
+
+
+//é a mesma coisa q fazer isso em baixo
+
+//const raiz = function(n) {
+//    return n ** 0.5;
+//};
+
+//console.log(raiz)
---
+++ Functions.js
@@ -0,0 +1,9 @@
+function soma ( x =1 , y=2)
+{
+    const resultado = x + y
+    return resultado;
+}
+
+const resultado = soma(4,3)
+
+console.log(resultado)
